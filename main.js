99bb8feaee4d64373bd4faba9339cfa256929161
StathisKotanidis <stathiskotanidhs@gmail.com>
2024-08-21 20:41:20 +0300

    finished the carousel

--- main.js
+++ main.js
@@ -17,31 +17,51 @@ function getCocktail() {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      console.log(data.drinks);
-      let cocktailIngredients = ''
-      let cocktailDescription = ''
-      data.drinks.forEach((element) => {
-        for (let i = 0; i <= 14; i++) {
-          if (element[`strIngredient${i + 1}`] != null) {
-            cocktailIngredients += element[`strIngredient${i + 1}`] + ', ';
-          }
+      console.log(data); //obj
+      console.log(data.drinks); //array of obj
+      console.log(data.drinks[0]); // 1st obj of the array
 
-        }
-        cocktailDescription += `
-          <div class="cocktail-description">
-              <h1>${element.strDrink}</h1>
-              <img src="${element.strDrinkThumb}" alt="cocktail-image">
-              <p>Ingredients: ${cocktailIngredients}</p>
-          </div>
-      `;
-        cocktailIngredients = ''
-      })
-      cocktailsContainer.innerHTML = cocktailDescription
-
+      updateCarousel(data, 0);
     })
     .catch((error) => {
       console.error("Fetch error", error);
     });
 }
 
+function getIngredients(obj, index) {
+  let ingredients = "";
+  for (let i = 1; i <= 15; i++) {
+    const ingredient = obj.drinks[index][`strIngredient${i}`];
+    if (ingredient != null && ingredient !== "") {
+      ingredients += ingredient + "<br>";
+    }
+  }
+
+  const cocktailDescription = `
+    <span class='carousel-arrows'><i id='left-arrow' class='bx bx-left-arrow-alt'></i></span>
+    <div class="cocktail-description">
+      <h1>${obj.drinks[index].strDrink}</h1>
+      <img src="${obj.drinks[index].strDrinkThumb}" alt="cocktail-image">
+      <p><span id="ingredients">Ingredients</span><br> ${ingredients}</p>
+    </div>
+    <span class='carousel-arrows'><i id='right-arrow' class='bx bx-right-arrow-alt'></i></span>
+  `;
+
+  return cocktailDescription;
+}
+
+function updateCarousel(obj, index) {
+  cocktailsContainer.innerHTML = getIngredients(obj, index);
+
+  document.querySelector("#right-arrow").addEventListener("click", () => {
+    if (index < obj.drinks.length - 1) {
+      updateCarousel(obj, index + 1);
+    }
+  });
+
+  document.querySelector("#left-arrow").addEventListener("click", () => {
+    if (index > 0) {
+      updateCarousel(obj, index - 1);
+    }
+  });
+}
